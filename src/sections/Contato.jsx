43d1a4bf621aa3e950ea9fdfd4e9059ec8540bee
Davyd Edivaldo Silva <davydedivaldo@gmail.com>
2025-12-27 11:26:41 -0300

    update

--- src/sections/Contato.jsx
+++ src/sections/Contato.jsx
@@ -13,7 +13,7 @@ const Contato = () => {
                 transition={{ duration: 0.6 }}
                 viewport={{ once: false, amount: 0.3 }}>
                 <h1 className="text-[35px] max-xl:text-xl font-bold bg-gradient-to-r from-[#a855f7] to-[#f0f0f0] bg-clip-text text-transparent mb-2">Contact</h1>
-                <p className="text-gray-300">
+                <p className="text-[23px] max-[800px]:text-[18px] text-gray-300">
                     Entre em contato comigo ou deixe um comentário.
                 </p>
             </motion.div>
